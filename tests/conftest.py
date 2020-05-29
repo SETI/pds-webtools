332193021b559e885952f9c3351773cf0ab0fde3
Yu-Jen Chang <juzen2003@gmail.com>
2020-05-28 18:45:21 -0700

    Update pytest command to take in --mode to select from on of the following four combinations: mode 1:     pdsfile.use_pickles(True)     pdsfile.use_shelves_only(True) mode 2:     pdsfile.use_pickles(True)     pdsfile.use_shelves_only(False) mode 3:     pdsfile.use_pickles(False)     pdsfile.use_shelves_only(True) mode 4:     pdsfile.use_pickles(False)     pdsfile.use_shelves_only(False)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,27 @@ PDS_DATA_DIR = os.environ['PDS_DATA_DIR']
 ################################################################################
 # Setup before all tests
 ################################################################################
+def pytest_addoption(parser):
+    parser.addoption("--mode", action="store")
+
 @pytest.fixture(scope='session', autouse=True)
-def setup():
+def setup(request):
+    mode = request.config.option.mode
+    # print(f'Current mode: {mode}')
+    # print(f'Current mode: {isinstance(mode, str)}')
+    # print(f'Current mode: {mode == "1"}')
     # pdsfile.use_pickles()
-    pdsfile.use_shelves_only()
+    # pdsfile.use_shelves_only()
+    if mode == '1':
+        pdsfile.use_pickles(True)
+        pdsfile.use_shelves_only(True)
+    elif mode == '2':
+        pdsfile.use_pickles(True)
+        pdsfile.use_shelves_only(False)
+    elif mode == '3':
+        pdsfile.use_pickles(False)
+        pdsfile.use_shelves_only(True)
+    elif mode == '4':
+        pdsfile.use_pickles(False)
+        pdsfile.use_shelves_only(False)
     pdsfile.preload(PDS_DATA_DIR)
