c06684f4599dcfa51afc55b53b6269f1e5f1afdb
Mia <15634946+mace-space@users.noreply.github.com>
2023-03-22 15:56:37 -0700

    Draft rule for uranus_occs_earthbased bundle set for opus_id #67

---
+++ Pds4File/pds4_rules/__init__.py
@@ -0,0 +1,10 @@
+################################################################################
+# rules/__init__.py
+#
+# Subclasses of PdsFile, encompassing dataset-specific information
+################################################################################
+
+__all__ = [
+    "uranus_occs_earthbased",
+]
+
