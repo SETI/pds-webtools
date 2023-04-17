dd70b520a20f5d9eea1ecd0b49d609feb955b838
Mia <15634946+mace-space@users.noreply.github.com>
2023-04-17 14:23:14 +0100

    Add tests for Cassini ISS Cruise and Cassini VIMS Cruise

--- Pds4File/tests/test_pds4file_blackbox.py
+++ Pds4File/tests/test_pds4file_blackbox.py
@@ -23,7 +23,23 @@ class TestPds4FileBlackBox:
              'uranus_occ_u0_kao_91cm'),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/rings/u0_kao_91cm_734nm_radius_alpha_egress_1000m.tab',
              'uranus_occ_u0_kao_91cm'),
-        ]
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             'co-iss-n1308947228'),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947880n.xml',
+             'co-iss-n1308947880'),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947440w.img',
+             'co-iss-w1308947440'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308946681_xxx/1308946681_002.qub',
+             'co-vims-v1308946681_002_vis'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.qub',
+             'co-vims-v1308947235_vis'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947009_xxx/1308947009_002.qub',
+             'co-vims-v1308947009_002_vis'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947715.xml',
+             'co-vims-v1308947715_vis'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947926_xxx/1308947926_008.qub',
+             'co-vims-v1308947926_008_vis'),
+    ]
     )
     def test_opus_id(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -35,7 +51,11 @@ class TestPds4FileBlackBox:
         [
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm',
              f'{PDS4_HOLDINGS_DIR}/uranus_occs_earthbased/uranus_occ_u0_kao_91cm'),
-        ]
+             ('cassini_iss/cassini_iss_cruise',
+             f'{PDS4_HOLDINGS_DIR}/cassini_iss/cassini_iss_cruise'),
+             ('cassini_vims/cassini_vims_cruise',
+             f'{PDS4_HOLDINGS_DIR}/cassini_vims/cassini_vims_cruise'),
+      ]
     )
     def test_abspath(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -47,7 +67,13 @@ class TestPds4FileBlackBox:
         [
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/rings/u0_kao_91cm_734nm_radius_alpha_egress_1000m.xml',
              'bundles/uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/rings/u0_kao_91cm_734nm_radius_alpha_egress_1000m.xml'),
-        ]
+             ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             'bundles/cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img'),
+             ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308946681_xxx/1308946681_002.qub',
+             'bundles/cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308946681_xxx/1308946681_002.qub'),
+             ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.qub',
+             'bundles/cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.qub'),
+     ]
     )
     def test_logical_path(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -61,7 +87,13 @@ class TestPds4FileBlackBox:
              True),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/rings/non-existent-filename.txt',
              False),
-        ]
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.xml',
+            True),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.xml',
+            True),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947009_xxx/1308947009_002.qub',
+            True),
+    ]
     )
     def test_exists(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -79,7 +111,25 @@ class TestPds4FileBlackBox:
              False),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/atmosphere/u0_kao_91cm_734nm_counts-v-time_atmos_egress.xml',
              False),
-        ]
+            ('cassini_iss',
+             ''), # bundlesets currently have empty string instead of False
+            ('cassini_iss/cassini_iss_cruise',
+             True),
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             False),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             False),
+            ('cassini_vims',
+             ''), # bundlesets currently have empty string instead of False
+            ('cassini_vims/cassini_vims_cruise',
+             True),
+            ('cassini_vims/cassini_vims_cruise/bundle.xml',
+             False),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.qub',
+             False),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947926_xxx/1308947926_008.xml',
+             False),
+     ]
     )
     def test_is_bundle(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -103,7 +153,41 @@ class TestPds4FileBlackBox:
              False),
             ('uranus_occs_earthbased/',
              ''), # bundlesets currently have empty string instead of False
-        ]
+            ('cassini_iss',
+             ''), # bundlesets currently have empty string instead of False
+            ('cassini_iss/cassini_iss_cruise',
+             True), 
+            ('cassini_iss/cassini_iss_cruise/browse',
+             False),
+            ('cassini_iss/cassini_iss_cruise/xml_schema/collection_xml_schema.csv',
+             False),
+            ('cassini_iss/cassini_iss_cruise',
+             True), 
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             False),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             False),
+            ('cassini_iss/cassini_iss_cruise/',
+             True), 
+            ('cassini_vims',
+             ''), # bundlesets currently have empty string instead of False
+            ('cassini_vims/cassini_vims_cruise',
+             True), 
+            ('cassini_vims/cassini_vims_cruise/calibration',
+             False),
+            ('cassini_vims/cassini_vims_cruise/xml_schema/collection_xml_schema.csv',
+             False),
+            ('cassini_vims/cassini_vims_cruise',
+             True), 
+            ('cassini_vims/cassini_vims_cruise/bundle.xml',
+             False),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947926_xxx/1308947926_008.qub',
+             False),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx//1308947715.xml',
+             False),
+            ('cassini_vims/cassini_vims_cruise/',
+             True), 
+      ]
     )
     def test_is_bundle_dir(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -117,7 +201,15 @@ class TestPds4FileBlackBox:
              False),
              ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/bundle.xml',
              False),
-        ]
+             ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             False),
+             ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             False),
+             ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947926_xxx/1308947926_008.qub',
+             False),
+             ('cassini_vims/cassini_vims_cruise/bundle.xml',
+             False),
+      ]
     )
     def test_is_bundle_file(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -131,9 +223,19 @@ class TestPds4FileBlackBox:
              True),
             ('uranus_occs_earthbased/',
              True),
-             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/bundle.xml',
+            ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/bundle.xml',
              False),
-        ]
+            ('cassini_iss',
+             True),
+            ('cassini_vims/',
+             True),
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             False),
+            ('cassini_iss/cassini_iss_cruise/',
+             False),
+            ('cassini_vims/cassini_vims_cruise',
+             False),
+      ]
     )
     def test_is_bundleset(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -149,7 +251,17 @@ class TestPds4FileBlackBox:
              True),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/bundle.xml',
              False),
-        ]
+            ('cassini_vims',
+             True),
+            ('cassini_iss/',
+             True),
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             False),
+            ('cassini_iss/cassini_iss_cruise',
+             False),
+            ('cassini_vims/cassini_vims_cruise',
+             False),
+       ]
     )
     def test_is_bundleset_dir(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -167,7 +279,25 @@ class TestPds4FileBlackBox:
              'uranus_occs_earthbased'),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/atmosphere/u0_kao_91cm_734nm_counts-v-time_atmos_ingress.tab',
              'uranus_occs_earthbased'),
-        ]
+            ('cassini_iss/',
+             'cassini_iss'),
+            ('cassini_iss/cassini_iss_cruise',
+             'cassini_iss'),
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             'cassini_iss'),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.img',
+             'cassini_iss'),
+            ('cassini_vims/',
+             'cassini_vims'),
+            ('cassini_vims/cassini_vims_cruise',
+             'cassini_vims'),
+            ('cassini_vims/cassini_vims_cruise/bundle.xml',
+             'cassini_vims'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947926_xxx/1308947926_008.qub',
+             'cassini_vims'),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947235.qub',
+             'cassini_vims'),
+     ]
     )
     def test_bundleset(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
@@ -186,7 +316,25 @@ class TestPds4FileBlackBox:
              []),
             ('uranus_occs_earthbased/uranus_occ_u0_kao_91cm/data/atmosphere/u0_kao_91cm_734nm_counts-v-time_atmos_egress.xml',
              []),
-        ]
+            ('cassini_iss/',
+             ['cassini_iss_cruise']),
+            ('cassini_iss/cassini_iss_cruise',
+             ['browse_raw', 'bundle.xml', 'context', 'data_raw', 'document','xml_schema']),
+            ('cassini_iss/cassini_iss_cruise/bundle.xml',
+             []),
+            ('cassini_iss/cassini_iss_cruise/data_raw/130xxxxxxx/13089xxxxx/1308947228n.xml',
+             []),
+            ('cassini_vims/',
+             ['cassini_vims_cruise']),
+            ('cassini_vims/cassini_vims_cruise',
+             ['browse_raw', 'bundle.xml', 'calibration', 'context', 'data_raw', 'document','xml_schema']),
+            ('cassini_vims/cassini_vims_cruise/bundle.xml',
+             []),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/1308946681_xxx/1308946681_002.qub',
+             []),
+            ('cassini_vims/cassini_vims_cruise/data_raw/130xxxxxxx/13089xxxxx/13089xxxxx/1308947235.xml',
+             []),
+     ]
     )
     def test_childnames(self, input_path, expected):
         target_pdsfile = instantiate_target_pdsfile(input_path)
