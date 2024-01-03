5254a64d78fd0bae322a9d7130b6ad9b61e91abc
Robert French <rfrench@seti.org>
2024-01-03 13:21:23 -0800

    Fixing uses of os.path.join

--- pdsfile.py
+++ pdsfile.py
@@ -5376,7 +5376,7 @@ class PdsFile(object):
             label_basename = self.label_basename
             if label_basename:
                 parent_abspath = os.path.split(self.abspath)[0]
-                label_abspath = os.path.join(parent_abspath, label_basename)
+                label_abspath = _clean_join(parent_abspath, label_basename)
                 if label_abspath not in abspaths:
                     abspaths.append(label_abspath)
 
@@ -5718,17 +5718,17 @@ def abspath_for_logical_path(path):
 
     # With exactly one holdings/ directory, the answer is easy
     if len(holdings_list) == 1:
-        return os.path.join(holdings_list[0], path)
+        return _clean_join(holdings_list[0], path)
 
     # Otherwise search among the available holdings directories in order
     for root in holdings_list:
-        abspath = os.path.join(root, path)
+        abspath = _clean_join(root, path)
         matches = PdsFile.glob_glob(abspath)
         if matches: return matches[0]
 
     # File doesn't exist. Just pick one.
     if holdings_list:
-        return os.path.join(holdings_list[0], path)
+        return _clean_join(holdings_list[0], path)
 
     raise ValueError('No holdings directory for logical path ' + path)
 
